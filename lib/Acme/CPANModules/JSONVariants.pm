ca66649c66d75daa440e64695c3f1cbf3ccb4caf
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2024-05-10 07:14:27 +0700

    Add jsonc, fix numbering, add website for each project

--- lib/Acme/CPANModules/JSONVariants.pm
+++ lib/Acme/CPANModules/JSONVariants.pm
@@ -21,45 +21,54 @@ This list catalogs the various JSON variants which have a Perl implementation on
 CPAN.
 
 
-**JSON5**. <https://json5.org/>, "JSON for Humans". Allowing more whitespaces,
-single-line comment (C++-style), multiline comment (C-style), single quote for
-strings, hexadecimal number literal (e.g. 0x123abc), leading decimal point,
-trailing decimal point, positive sign in number, trailing commas.
+1) **JSON5**. <https://json5.org/>, "JSON for Humans". Allowing more
+whitespaces, single-line comment (C++-style), multiline comment (C-style),
+single quote for strings, hexadecimal number literal (e.g. 0x123abc), leading
+decimal point, trailing decimal point, positive sign in number, trailing commas.
 
 Perl modules: <pm:JSON5>, <pm:File::Serialize::Serializer::JSON5>.
 
 
-1) **HJSON**. <https://hjson.org>, Human JSON. A JSON variant that aims to be
+2) **HJSON**. <https://hjson.org>, Human JSON. A JSON variant that aims to be
 more user-friendly by allowing comments, unquoted keys, and optional commas.
 It's designed to be easier to read and write by humans.
 
 Perl modules: (none so far).
 
 
-2) **JSONC**. JSON with Comments.
+3) **JSONC**. <https://github.com/komkom/jsonc>. JSON with Comments. Also allows
+unquoted string values with whitespace as delimiters.
 
 Perl modules: (none so far).
 
 
-3) **CSON**. CofeeScript Object Notation. JSON-like data serialization format
-inspired by CoffeeScript syntax. It allows for a more concise representation of
-data by leveraging CoffeeScript's features such as significant whitespace and
-optional commas.
+4) **jsonc**. <https://code.visualstudio.com/docs/languages/json#_json-with-comments>.
+Another "JSON with Comments", supported by Microsoft Visual Code.
 
 Perl modules: (none so far).
 
 
-4) **RJSON**. Relaxed JSON. Trailing commas, comments (C-style and C++-style),
-single-quoted strings as well as bare/unquoted, hash key without value (value
-will default to `undef`).
+5) **CSON**. <https://github.com/bevry/cson>. CofeeScript Object Notation.
+JSON-like data serialization format inspired by CoffeeScript syntax. It allows
+for a more concise representation of data by leveraging CoffeeScript's features
+such as significant whitespace and optional commas.
+
+Perl modules: (none so far).
+
+
+6) **RJSON**. <https://relaxedjson.org/>. Relaxed JSON. Trailing commas,
+Optional commas, comments (C-style and C++-style), single-quoted &
+backtick-quoted strings as well as bare/unquoted, hash key without value (value
+will default to `undef`). It touts itself as "going beyond other JSON variants,
+including JSON5."
 
 Perl modules: <pm:JSON::Relaxed>.
 
 
-5) **<pm:JSON::Diffable>**. Basically just allowing for trailing commas.
+7) **<pm:JSON::Diffable>**. Basically just allowing for trailing commas.
 
 
-6) **JSONLines**. <https://jsonlines.org>. A more restrictive JSON format, all
+8) **JSONLines**. <https://jsonlines.org>. A more restrictive JSON format, all
 JSON records must fit in one line as newline is the record delimiter. Encoding
 must be UTF-8. Convention for line-oriented processing which support JSON. E.g.
 for CSV replacement.
@@ -67,8 +76,8 @@ for CSV replacement.
 Perl moduless: <pm:JSON::Lines>.
 
 
-7) **YAML**. YAML is a superset of JSON. It allows for indentation-based syntax
-and various features like references, heredocs, etc.
+9) **YAML**. <https://yaml.org/>. YAML is a superset of JSON. It allows for
+indentation-based syntax and various features like references, heredocs, etc.
 
 Perl modules: <pm:YAML>, <pm:YAML::PP>, among others.
 
